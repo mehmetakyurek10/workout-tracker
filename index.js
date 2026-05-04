64bb24eac4a117229b73f7a035216a810e2fe27c
mehmetakyurek1 <151149491+mehmetakyurek10@users.noreply.github.com>
2026-05-04 22:27:48 +0300

    feat: add form to create new exercises with MySQL INSERT

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config()
 const db = require("./data/db")
-
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
 app.set("view engine", "ejs")
 const path = require("path")
 app.use(express.static(path.join(__dirname,"public")))
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req,res) => {
     res.send("Workout Tracker Hazır Ve Nazır")
@@ -33,6 +33,29 @@ app.get("/egzersizler", async(req,res) => {
         console.log(err)
         res.status(500).send("DB Hatası")
     }
+})
+
+app.get("/egzersizler/ekle",(req,res) => {
+    res.render("egzersizler/ekleme")
+})
+
+app.post("/egzersizler/ekle",async(req,res) => {
+    const {isim,kas_grubu,ekipman,aciklama} = req.body
+    try{
+        await db.execute(
+            "INSERT INTO egzersizler (isim,kas_grubu,ekipman,aciklama) VALUES (?,?,?,?)",
+            [isim,kas_grubu,ekipman,aciklama]
+        )
+        res.redirect("/egzersizler")
+   
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send("DB Hatası")
+    }
+    
+    
+
 })
 
 app.listen(PORT, () => {
